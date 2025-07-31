b1c97d8c303ede9f661fa2e1d9f7e2b6375207ee
janphilippgutt <janphilippgutt@posteo.de>
2025-07-31 14:18:41 +0200

    Delete optional config from auth module

--- modules/auth/main.tf
+++ modules/auth/main.tf
@@ -18,9 +18,6 @@ resource "aws_cognito_user_pool_client" "no_secret_client" {
 
   prevent_user_existence_errors = "ENABLED"
 
-  # Optional: Useful for Hosted UI / OAuth (can be left out if unused)
-  callback_urls = ["http://localhost:3000"]
-  logout_urls   = ["http://localhost:3000"]
 
   allowed_oauth_flows_user_pool_client = true
   allowed_oauth_flows                  = ["code", "implicit"]
