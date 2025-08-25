612b778b30aa663a848bac2416e27b303d2cb1dd
janphilippgutt <janphilippgutt@posteo.de>
2025-08-25 12:25:16 +0200

    Reproducibility adjustment: Call s3 static site module with random suffix instead of hard-coded bucket name

--- modules/deploy-frontend/main.tf
+++ modules/deploy-frontend/main.tf
@@ -1,6 +1,10 @@
+resource "random_id" "bucket_id" {
+  byte_length = 4
+}
+
 module "s3_static_site" {
   source               = "git::https://github.com/janphilippgutt/terraform-aws-modules.git//modules/s3-static-site?ref=main"
-  bucket_name          = "secure-file-share-demo-1234565000" # must be globally unique
+  bucket_name          = "secure-file-share-static-site-demo${random_id.bucket_id}"
   environment          = "dev"
   enable_public_access = false
   use_oac              = true
